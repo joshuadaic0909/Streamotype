0ad9140739efe7a0e500f9248f8af22d451979ea
Joshua Dai <joshuadaic@gmail.com>
2023-09-20 22:47:20 -0700

    delete

--- app/main/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template, request
-from app.main import bp 
-import pandas as pd
-
-from config import Config
-
-@bp.route('/')
-def index():
-    return render_template('index.html')
